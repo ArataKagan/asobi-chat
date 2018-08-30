633915b4cf4d9a8d29a29693d45212ee7c8df8ef
Arata Kagan <aratakagan@gmail.com>
2018-08-29 17:40:14 -0700

    list chat room: in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCphZRFH4a5RmE_poq1R_-CEe3trxj6qIg",
+    authDomain: "bloc-chat-react-3b3db.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-3b3db.firebaseio.com",
+    projectId: "bloc-chat-react-3b3db",
+    storageBucket: "bloc-chat-react-3b3db.appspot.com",
+    messagingSenderId: "312732468411"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
