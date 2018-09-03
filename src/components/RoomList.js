15db73d2265d09ef10d3e55c36b0f66e0d5d8bdd
Arata Kagan <aratakagan@gmail.com>
2018-09-02 18:14:15 -0700

    assignment-create-chat-room: in progress

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,32 +1,82 @@
 import React, { Component } from 'react';
+import Popup from 'reactjs-popup';
+
 
 class RoomList extends Component{
   constructor(props){
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoom : '',
+      open: false
     };
-
+    //accessing data from firebase
     this.roomsRef = this.props.firebase.database().ref('rooms');
     };
 
     componentDidMount(){
+      // each room is stored in state inital 
       this.roomsRef.on('child_added', snapshot => {
         const room = snapshot.val();
-        console.log(room);
         room.key = snapshot.key;
+        //console.log(room.key);
         this.setState({ rooms: this.state.rooms.concat( room )})
       });
     }
 
+    openModal = () => {
+      this.setState({open: true});
+    };
+
+    closeModal = () => {
+      this.setState({open: false})
+    };
+
+    createRoom(e){
+      // console.log('room created')
+      e.preventDefault();
+      console.log(this.state.newRoom);
+      
+      this.roomRef.push({
+        name: this.state.newRoom
+      });
+
+      this.setState({newRoom:' '});
+    };
+
+    handleChange(e){
+      console.log('room created');
+      this.setState({ newRoom: e.target.value });
+    }
+
     render(){
       return(
-        this.state.rooms.map((room, index) =>
-          <p key={index}>{room.name}</p>
-        )
-      );
-    }
+        <div>
+          <button className='room_btn' onClick={this.openModal}>Create New Room</button>
+          <Popup
+            open={this.state.open}
+            closeOnDocumentClick
+            onClose={this.closeModal}>
+              <form onSubmit={(e) => this.createRoom(e) } >
+                <label>
+                  Enter a room name:
+                  <input type="text" value={this.state.newRoom} onChange={(e) => this.handleChange(e)} />
+                  </label>
+                <input type='submit' />
+              </form>
+          </Popup>
+
+          <li className='room_list'>
+            {this.state.rooms.map((room, index) =>
+              <ul key={index}>{room.name}</ul>
+            )}
+          </li>
+
+        </div>
+    );
   }
+}
 
 
 export default RoomList;
+
