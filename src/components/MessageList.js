7cbc8ec5b6b5140f9dfed7301139ed7ecb96b82b
Arata Kagan <aratakagan@gmail.com>
2018-09-06 09:26:17 -0700

    list message assignment: Finished

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -13,27 +13,21 @@ class MessageList extends Component {
     componentDidMount(){
         this.messageRef.on('child_added', snapshot => {
             // store the entire message into newPost 
-            console.log('passed to message list')
             // store the entire message info into newPost
             const newPost = snapshot.val();
             newPost.key = snapshot.key; 
-            console.log(newPost); 
-            console.log(newPost.roomId);
             // store the message to initial state 
             this.setState({messages: this.state.messages.concat(newPost)});
-            console.log('message was stored in messagelist')
-            console.log(this.state.messages)
         })   
-    }
+    } 
 
     render(){
         return(
             <div className='messageList'>
-                <p>{this.props.activeRoomKeyfromParent}</p>
-                <ul>
-                    {this.state.messages.filter((item) => item.roomId === this.props.activeRoomKeyfromParent)
-                        .map((room, index) => ( <li key='index'> {room.username} {room.content} </li> ))}
-                </ul>
+                {this.state.messages.filter((item) => item.roomId === this.props.activeRoomKeyfromParent)
+                        .map((room, index) => ( 
+                        <p key='index'> <b>{room.username}</b> <p>{room.content}</p> </p> 
+                        ))}
             </div>
         )
     }
